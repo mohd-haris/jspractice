9bf2c0c2e719d0306c68e909336e289d279d1fc1
mohd-haris <mohd.haris@robomq.io>
2020-01-12 18:06:41 +0530

    commit 2

---
+++ webCheck.js
@@ -0,0 +1,16 @@
+let useremail = '123'
+
+
+let emailCheck = function(email){
+
+    
+    if ((email.includes(123)) && (email.length > 3))
+    {
+        return true
+    }
+
+    return false
+
+}
+
+console.log(emailCheck(useremail))
